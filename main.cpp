516178cd428f688d068a344c1d5b416da5d1eb10
nepgya <nguyenquangtrieu99@gmail.com>
2024-07-25 02:20:35 +0700

    Add main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
-#include <iomanip>
 #include <iostream>
 
 #define LOG(ITEMS) std::cout << ITEMS << std::endl
 
 int main()
 {
-  std::cout << "Welcome to the farm" << std::endl;
-  while (true)
-  {
-    LOG("Enter your choice: ");
-    LOG("");
-  }
+    std::cout << "Welcome to the farm" << std::endl;
+    while (true)
+    {
+        LOG("Enter your choice: ");
+        LOG("1. Report all");
+        LOG("2. Report resource");
+        LOG("3. Report animals");
+        LOG("4. Feed animals");
+        LOG("5. Let animals out");
+        LOG("6. Buy animals");
+        LOG("7. Sell animals");
+        LOG("8. Quit the game");
+    }
 }
