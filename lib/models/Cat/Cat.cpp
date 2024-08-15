33b4dbe83c3588a7789e72aa5fb2cb9e321ef4fa
nepgya <nguyenquangtrieu99@gmail.com>
2024-08-15 23:12:05 +0700

    Change clang format

--- lib/models/Cat/Cat.cpp
+++ lib/models/Cat/Cat.cpp
@@ -1,40 +1,32 @@
 #include "Cat.hpp"
 
-namespace Entity
-{
-    Cat::Cat(const int& Name, const int& Age, const int& Weight,
-             const int& HappyIndex, const int& Price)
-    {
-        this->Name = Name;
-        this->Age = Age;
-        this->Weight = Weight;
-        this->HappyIndex = HappyIndex;
-        this->Price = Price;
-    }
+namespace Entity {
+	Cat::Cat(const int &Name, const int &Age, const int &Weight, const int &HappyIndex, const int &Price) {
+		this->Name       = Name;
+		this->Age        = Age;
+		this->Weight     = Weight;
+		this->HappyIndex = HappyIndex;
+		this->Price      = Price;
+	}
 
-    int Cat::Sound()
-    {
-        return -1;
-    }
+	int Cat::Sound() {
+		return -1;
+	}
 
-    int Cat::Eat()
-    {
-        return -1;
-    }
+	int Cat::Eat() {
+		return -1;
+	}
 
-    int Cat::Reproduce()
-    {
-        return -1;
-    }
+	int Cat::Reproduce() {
+		return -1;
+	}
 
-    int Cat::GoOut()
-    {
-        return -1;
-    }
+	int Cat::GoOut() {
+		return -1;
+	}
 
-    int Cat::Die()
-    {
-        return -1;
-    }
+	int Cat::Die() {
+		return -1;
+	}
 
 } // namespace Entity
